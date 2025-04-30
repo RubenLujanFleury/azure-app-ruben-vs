ca77ef3b605209f4490c4141e627cbd6c24401b4
Ruben Lujan <rluja075@uottawa.ca>
2025-04-30 17:04:16 -0400

    Added EF Core and Azure SQL Database Connection

--- azure-app-ruben-vs/Program.cs
+++ azure-app-ruben-vs/Program.cs
@@ -1,7 +1,14 @@
 
+using azure_app_ruben_vs.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("AzureSqlConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
 builder.Services.AddRazorPages();
 builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
 {
